51744d369a2f10d0e65f82983f5a179434b59161
NeerajKumar09 <neerajkumar09@NeerajKumar09s-Mac-mini.local>
2025-11-25 23:08:24 +0530

    Automation commit

---
+++ tests/test_google.py
@@ -0,0 +1,14 @@
+
+import re
+from playwright.sync_api import  expect
+
+
+def test_google_page(page):
+    page.wait_for_timeout(4000)
+    page.goto("https://www.google.com/")
+    page.get_by_role("combobox",name = "search").fill("playwright Python")
+    page.keyboard.press("Enter")
+    expect(page).to_have_title(re.compile("Playwright",re.IGNORECASE))
+
+
+
